fd2c4c258be324f88840de203e47d05cd8ab7b20
cloudbrownie <andynguyen.aln@gmail.com>
2021-02-25 12:47:33 -0500

    *TEST* Added a new test instruction that uses set power
    Instruction uses an internal timer to run, time is based on a conversion of inches into seconds using the max motor speed in ticks/second. Test instruction also implements speeding up and slowing down and needs testing for the drive multiplier.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousInstructions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousInstructions.java
@@ -18,7 +18,13 @@ public class AutonomousInstructions {
     private final int WHEEL_DIAM = 4;
     private final double INCHES_PER_REVOLUTION = WHEEL_DIAM * Math.PI;
     private final double TICKS_PER_INCHES = TICKS_PER_REVOLUTION / INCHES_PER_REVOLUTION;
+    private final double INCHES_PER_TICK = 1 / TICKS_PER_INCHES;
     private long drivingDelay = 0;
+
+    private final int HD_HEX_MOTOR_MAX_RPM = 300;
+    private final int MAX_TICKS_PER_SECOND = (HD_HEX_MOTOR_MAX_RPM / 60) * TICKS_PER_REVOLUTION;
+    private final int PROPORTIONATE_TICKS_PER_SECOND = (int)(MAX_TICKS_PER_SECOND * speed);
+    private final double PROPORTIONATE_INCHES_PER_SECOND = PROPORTIONATE_TICKS_PER_SECOND * INCHES_PER_TICK;
 
     // these keys make typing them into the add instruction methods easier to autocomplete
     public final int driveForward = 0;
@@ -27,6 +33,10 @@ public class AutonomousInstructions {
     public final int strafeRight = 3;
     public final int turnLeft = 4;
     public final int turnRight = 5;
+
+    // driving multipliers
+    private final double driveMultiplier = 1.07;
+    private final double strafeMultiplier = 1.25;
 
     // instruction list
     ArrayList<SequentialInstruction> seqInstructions = new ArrayList<>();
@@ -238,10 +248,10 @@ public class AutonomousInstructions {
             super(delay);
             this.driveMotors = driveMotors;
             if (methodKey == strafeLeft || methodKey == strafeRight) {
-                inches *= 1.25;
+                inches *= strafeMultiplier;
             }
             else if (methodKey == driveForward || methodKey == driveBackward) {
-                inches *= 1.07;
+                inches *= driveMultiplier;
             }
             this.distance = (int)Math.round(inches * TICKS_PER_INCHES);
             this.method = methodKey;
@@ -427,6 +437,201 @@ public class AutonomousInstructions {
     // timer instructions --------------------------------------------------------------------------
 
 
+    // test instructions based on motor max speed --------------------------------------------------
+    private class TestMotorDrive extends SequentialInstruction {
+        private final ArrayList<DcMotor> driveMotors;
+        private final double duration;
+        private final int method;
+
+        // used for speeding up and slowing down
+        private double speedMultiplier;
+        private long driveStartTime;
+        private long slowStartTime;
+        private boolean slowingDown = false;
+        private final double timeThreshold;
+
+        private final double millisToSpeedUp = 500;
+        private final double millisToSlowDown = 1000;
+        private final double base = .15;
+
+        public TestMotorDrive(long delay, ArrayList<DcMotor> driveMotors, double inches, int methodKey) {
+            super(delay);
+            this.driveMotors = driveMotors;
+            this.method = methodKey;
+            // multiplier for the inches based on methodkey
+            if (methodKey == driveForward || methodKey == driveBackward) {
+                inches *= driveMultiplier;
+            }
+            else if (methodKey == strafeLeft || methodKey == strafeRight) {
+                inches *= strafeMultiplier;
+            }
+
+            // convert inches into time
+            this.duration = inches * PROPORTIONATE_INCHES_PER_SECOND;
+
+            // create time threshold
+            this.timeThreshold = this.duration - this.millisToSlowDown;
+        }
+
+        public void Perform() {
+            // iterate through each motor
+            for (DcMotor motor: this.driveMotors) {
+                // set to run with encoders (in case they weren't)
+                motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+
+            // switch case to set the correct powers for the different driving methods
+            switch (this.method) {
+                case driveForward:
+                    for (DcMotor motor: this.driveMotors) {
+                        motor.setPower(speed);
+                    }
+                    break;
+                case driveBackward:
+                    for (DcMotor motor: this.driveMotors) {
+                        motor.setPower(-speed);
+                    }
+                    break;
+                case strafeLeft:
+                    this.driveMotors.get(0).setPower(-speed);
+                    this.driveMotors.get(1).setPower(speed);
+                    this.driveMotors.get(2).setPower(speed);
+                    this.driveMotors.get(3).setPower(-speed);
+                    break;
+                case strafeRight:
+                    this.driveMotors.get(0).setPower(speed);
+                    this.driveMotors.get(1).setPower(-speed);
+                    this.driveMotors.get(2).setPower(-speed);
+                    this.driveMotors.get(3).setPower(speed);
+                    break;
+            }
+
+            // set the start time
+            this.driveStartTime = System.currentTimeMillis();
+        }
+
+        public void Check(double correction) {
+            final long elapsedTime = System.currentTimeMillis() - this.driveStartTime;
+            // check if dead
+            if (elapsedTime > this.duration) {
+                this.dead = true;
+            }
+
+            // otherwise perform routine
+            else {
+
+                // test if we need to start slowing down
+                if (elapsedTime > this.timeThreshold) {
+                    this.slowStartTime = System.currentTimeMillis();
+                    this.slowingDown = true;
+                }
+
+                // adjust multiplier
+                this.AdjustMultiplier();
+
+                // switch case since the sign of the speed value determines motor spin direction
+                switch (this.method) {
+                    case driveForward:
+                        this.driveMotors.get(0).setPower((speed - correction));
+                        this.driveMotors.get(1).setPower((speed + correction));
+                        this.driveMotors.get(2).setPower((speed - correction));
+                        this.driveMotors.get(3).setPower((speed + correction));
+                        break;
+                    case driveBackward:
+                        this.driveMotors.get(0).setPower((-speed - correction));
+                        this.driveMotors.get(1).setPower((-speed + correction));
+                        this.driveMotors.get(2).setPower((-speed - correction));
+                        this.driveMotors.get(3).setPower((-speed + correction));
+                        break;
+                    case strafeLeft:
+                        this.driveMotors.get(0).setPower((-speed - correction));
+                        this.driveMotors.get(1).setPower((speed + correction));
+                        this.driveMotors.get(2).setPower((speed - correction));
+                        this.driveMotors.get(3).setPower((-speed + correction));
+                        break;
+                    case strafeRight:
+                        this.driveMotors.get(0).setPower((speed - correction));
+                        this.driveMotors.get(1).setPower((-speed + correction));
+                        this.driveMotors.get(2).setPower((-speed - correction));
+                        this.driveMotors.get(3).setPower((speed + correction));
+                        break;
+                }
+
+                // apply speed multiplier
+                for (DcMotor motor: this.driveMotors) {
+                    motor.setPower(motor.getPower() * this.speedMultiplier);
+                }
+            }
+        }
+
+        // speed multiplier function
+        private void AdjustMultiplier() {
+            double value;
+
+            /*final double twoSeocndsQuadraticConstantA = 2.5 * Math.pow(10, -7)
+            if (this.slowingDown) {
+                value = 1 - twoSeocndsQuadraticConstantA * Math.pow(System.currentTimeMillis() - this.slowStartTime, 2);
+            }
+            else {
+                value = twoSeocndsQuadraticConstantA * Math.pow(System.currentTimeMillis() - this.driveStartTime, 2) + this.base;
+            }*/
+
+            // test
+            if (this.slowingDown) {
+                value = this.SlowDown();
+            }
+            else {
+                value = this.SpeedUp();
+            }
+
+            this.speedMultiplier = value;
+
+            if (this.speedMultiplier > 1) {
+                this.speedMultiplier = 1;
+            }
+            else if (this.speedMultiplier < this.base) {
+                this.speedMultiplier = this.base;
+            }
+        }
+
+        // quadratic formulas for slowing down and speeding up
+        private double SlowDown() {
+            // define the three points
+            final double x1 = -this.millisToSlowDown;
+            final double y1 = this.base;
+            final double x2 = 0;
+            final double y2 = 1;
+            final double x3 = this.millisToSlowDown;
+            final double y3 = this.base;
+
+            final double elapsedTime = System.currentTimeMillis() - this.slowStartTime;
+
+            // return y
+            return ((elapsedTime-x2) * (elapsedTime-x3)) / ((x1-x2) * (x1-x3)) * y1 +
+                    ((elapsedTime-x1) * (elapsedTime-x3)) / ((x2-x1) * (x2-x3)) * y2 +
+                    ((elapsedTime-x1) * (elapsedTime-x2)) / ((x3-x1) * (x3-x2)) * y3;
+        }
+
+        private double SpeedUp() {
+            // define the three points
+            // constants used for speeding up and slowing down
+            final double x1 = -this.millisToSpeedUp;
+            final double y1 = 1;
+            final double x2 = 0;
+            final double y2 = this.base;
+            final double x3 = this.millisToSpeedUp;
+            final double y3 = 1;
+
+            final double elapsedTime = System.currentTimeMillis() - this.driveStartTime;
+
+            // return y
+            return ((elapsedTime-x2) * (elapsedTime-x3)) / ((x1-x2) * (x1-x3)) * y1 +
+                    ((elapsedTime-x1) * (elapsedTime-x3)) / ((x2-x1) * (x2-x3)) * y2 +
+                    ((elapsedTime-x1) * (elapsedTime-x2)) / ((x3-x1) * (x3-x2)) * y3;
+        }
+    }
+
+
     // called in op mode methods -------------------------------------------------------------------
     // called inside the autonomous program's while loop
     public void HandleInstructions(long elapsedTime, double correction) {
@@ -481,4 +686,7 @@ public class AutonomousInstructions {
         seqInstructions.add(new SequentialDrivingInstruction(delay, driveMotors, inches, methodKey));
     }
 
+    public void AddTestDrive(long delay, ArrayList<DcMotor> driveMotors, double inches, int methodKey) {
+        seqInstructions.add(new TestMotorDrive(delay, driveMotors, inches, methodKey));
+    }
 }
